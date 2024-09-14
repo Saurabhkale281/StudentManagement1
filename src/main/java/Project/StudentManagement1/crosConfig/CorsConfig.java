10c2bca1d34259211708aea05383bc29031e212d
Saurabh Kale <kalesaurabh281@gmail.com>
2024-09-14 11:39:12 +0530

    change cors origin

--- src/main/java/Project/StudentManagement1/crosConfig/CorsConfig.java
+++ src/main/java/Project/StudentManagement1/crosConfig/CorsConfig.java
@@ -11,7 +11,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")									// Configures Cross Origin Resource Sharing for all ends points 
-                .allowedOrigins("https://student-management-mu.vercel.app")			//allowed origins to access resources
+                .allowedOrigins("https://student-management-mu.vercel.app/")			//allowed origins to access resources
                 .allowedMethods("GET", "POST", "PUT", "DELETE")		//HTTP methods that are allowed for CORS requests
                 .allowedHeaders("*")
                 .allowCredentials(true);							//browser should include credentials likes cookies, HTTP authentication in CORS requests we make true
