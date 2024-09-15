906ed25f047524c1adde7ee614ff9d2ea4b89a94
Saurabh Kale <kalesaurabh281@gmail.com>
2024-09-15 23:01:34 +0530

    update corsConfig

--- src/main/java/Project/StudentManagement1/crosConfig/CorsConfig.java
+++ src/main/java/Project/StudentManagement1/crosConfig/CorsConfig.java
@@ -8,13 +8,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
 	
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")									// Configures Cross Origin Resource Sharing for all ends points 
-        .allowedOrigins("http://127.0.0.1:5500",              // Allow local development frontend
-                "https://student-management-mu.vercel.app") 			//allowed origins to access resources
-                .allowedMethods("GET", "POST", "PUT", "DELETE")		//HTTP methods that are allowed for CORS requests
-                .allowedHeaders("*")
-                .allowCredentials(true);							//browser should include credentials likes cookies, HTTP authentication in CORS requests we make true
-    }
+	 @Override
+	    public void addCorsMappings(CorsRegistry registry) {
+	        registry.addMapping("/**")
+	                .allowedOrigins("https://student-management-mu.vercel.app") // Allow frontend domain
+	                .allowedMethods("GET", "POST", "PUT", "DELETE") // Allowed HTTP methods
+	                .allowedHeaders("*") // Allow all headers
+	                .allowCredentials(true); // Allow credentials if needed
+	    }
 }
